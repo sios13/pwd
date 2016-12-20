7adedfd610c898ed5298d23fe76ae723a1aae28f
Simon <simon2@live.se>
2016-12-20 17:18:34 +0100

    memory can now choose nr of pairs.

--- examination/client/source/js/apps/Memory/MemoryGame.js
+++ examination/client/source/js/apps/Memory/MemoryGame.js
@@ -1,7 +1,74 @@
 const MemoryGameBoard = require("./MemoryGameBoard.js");
 
 function MemoryGame(config) {
+    //new MemoryGameBoard(config);
+    /**
+     * Properties
+     */
+    let container = config.container ? config.container : "#123";
+
+    /**
+     * Elements
+     */
+    //Wrapper
+    let memoryWrapperTemplate = document.querySelector("#memoryWrapperTemplate");
+    let memoryWrapperFrag = document.importNode(memoryWrapperTemplate.content, true);
+
+    let memoryWrapperDiv = memoryWrapperFrag.querySelector(".Memory-wrapper");
+
+    // Pair form
+    let memoryPairFormTemplate = document.querySelector("#memoryPairFormTemplate");
+    let memoryPairFormFrag = document.importNode(memoryPairFormTemplate.content, true);
+
+    let memoryPairForm = memoryPairFormFrag.querySelector("#memoryPairForm");
+    memoryWrapperDiv.appendChild(memoryPairForm);
+
+    // Radio inputs
+    let memoryPairRadioTemplate = document.querySelector("#memoryPairRadioTemplate");
+
+    for (let i = 1; i <= 8; i++) {
+        let memoryPairRadioFrag = document.importNode(memoryPairRadioTemplate.content, true);
+
+        let memoryPairLabel = memoryPairRadioFrag.querySelector(".memoryPairRadioLabel");
+        let memoryPairRadio = memoryPairLabel.querySelector("input");
+
+        memoryPairLabel.appendChild(document.createTextNode(i + " pairs"));
+        // Fix to make radio inputs clickable
+        memoryPairLabel.addEventListener("click", function(e) {
+            e.stopPropagation();
+            this.click();
+        });
+        memoryPairRadio.setAttribute("value", i);
+
+        memoryPairForm.appendChild(memoryPairLabel);
+        memoryPairForm.appendChild(document.createElement("br"));
+    }
+
+    // Form button
+    let memoryPairFormButtonTemplate = document.querySelector("#memoryPairFormButtonTemplate");
+    let memoryPairFormButtonFrag = document.importNode(memoryPairFormButtonTemplate.content, true);
+
+    let memoryPairFormButton = memoryPairFormButtonFrag.querySelector("button");
+    memoryPairFormButton.addEventListener("click", memoryPairFormButtonEvent);
+
+    memoryPairForm.appendChild(memoryPairFormButton);
+
+    // Container
+    let memoryContainerDiv = document.querySelector(container);
+    memoryContainerDiv.appendChild(memoryWrapperDiv);
+    /*
+    memoryContainerDiv.textContent = "";
     new MemoryGameBoard(config);
+    */
+
+    function memoryPairFormButtonEvent() {
+        //alert(document.querySelector("#memoryPairForm input:checked").value);
+        let nrOfPairs = document.querySelector("#memoryPairForm input:checked").value;
+
+        memoryContainerDiv.textContent = "";
+        config.nrOfPairs = parseInt(nrOfPairs);
+        new MemoryGameBoard(config);
+    }
 }
 
 module.exports = MemoryGame;
