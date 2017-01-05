62a7909243c8367fbb5e1aa792f7432ac42e33ac
Simon <simon2@live.se>
2017-01-05 13:22:14 +0100

    working on settings.

--- examination/client/source/js/apps/Settings/Settings.js
+++ examination/client/source/js/apps/Settings/Settings.js
@@ -5,10 +5,41 @@ function Settings(settings = {}) {
        "api": settings.api
     });
 
+    /**
+     * Elements
+     */
     this.container = settings.container ? settings.container : undefined;
 
     this.windowDiv = document.querySelector(this.container);
     this.windowDiv.classList.add("settings");
+
+    /**
+     * Background
+     */
+    this.backgroundForm = document.createElement("form");
+    this.backgroundForm.classList.add("settingsBackgroundForm");
+
+    for (let i = 0; i < 6; i++) {
+        let formLabel = document.createElement("label");
+        formLabel.setAttribute("for", "backgroundForm" + i);
+
+        let formRadio = document.createElement("input");
+        formRadio.setAttribute("type", "radio");
+        formRadio.setAttribute("value", i);
+        formRadio.setAttribute("name", "background");
+        formRadio.setAttribute("id", "backgroundForm" + i);
+
+        let formImage = document.createElement("img");
+        formImage.setAttribute("src", "./image/0.png");
+        formImage.setAttribute("alt", "Background");
+
+        formLabel.appendChild(formRadio);
+        formLabel.appendChild(formImage);
+
+        this.backgroundForm.appendChild(formLabel);
+    };
+
+    this.windowDiv.appendChild(this.backgroundForm);
 
     this.api.setPwdBackground(0);
     this.api.setPwdBackground(1);
